3742549e40f4f0eaee8ff3056e49d297c42feb6c
madison-howitt <159974224+madison-howitt@users.noreply.github.com>
2024-11-05 09:31:29 -0800

    Update shifter.sv

--- shifter.sv
+++ shifter.sv
@@ -1,5 +1,5 @@
 module shifter(in,shift,sout);
-  //declerations of inputs and output
+  //declarations of inputs and output
   input [15:0] in; // The 16-bit input that is to be shifted
   input [1:0] shift; //The 2-bit contol signal for the shift operation
   output reg [15:0] sout; //The 16-bit shifted output
