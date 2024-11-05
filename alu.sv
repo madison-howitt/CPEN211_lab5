235f6c179597c75c185ba93353cfe011c5331f8f
madison-howitt <159974224+madison-howitt@users.noreply.github.com>
2024-11-04 19:42:29 -0800

    Update alu.sv

--- alu.sv
+++ alu.sv
@@ -1,18 +1,18 @@
 module ALU(Ain,Bin,ALUop,out,Z);
   // declarations from Lab 5 outline
-  input signed [15:0] Ain, Bin;
+  input [15:0] Ain, Bin;
   input [1:0] ALUop;
-  output signed reg [15:0] out;
+  output reg [15:0] out;
   output reg Z;
 
   always_comb begin 
-    case (ALUop) 
-      2'b00: out = Ain + Bin; 
-      2'b01: out = Ain + (~Bin + 1'b1); 
-      2'b10: out = Ain & Bin; 
-      default: out = ~Bin; 
+    case (ALUop)   // preforms operations based on the value of ALUop
+      2'b00: out = Ain + Bin;             // addition
+      2'b01: out = Ain + (~Bin + 1'b1);   // subtraction using 2's complement
+      2'b10: out = Ain & Bin;             // bit-wise AND
+      default: out = ~Bin;                // bit-wise NOT using 2's complement, ~Bin = -Bin - 1
     endcase 
-    Z = (out == 16'b0) ? 1 : 0;
+    Z = (out == 16'b0) ? 1 : 0;   // if the output is 0, Z = 1
   end 
   
 endmodule
