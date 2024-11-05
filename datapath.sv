444c995e5cee424fc44337c51dcba228cd30a0a3
madison-howitt <159974224+madison-howitt@users.noreply.github.com>
2024-11-04 21:08:56 -0800

    Update datapath.sv

--- datapath.sv
+++ datapath.sv
@@ -1,25 +1,21 @@
 module datapath(datapath_in, vsel, writenum, write, readnum, clk, loada, loadb, shift, asel, bsel, ALUop, loads, loadc, Z_out, datapath_out);
-
-  Z_out, datapath_out; 
-  
   input [15:0] datapath_in; 
   input [2:0] writenum, readnum;
   input [1:0] shift, ALUop; 
   input vsel, write, clk, loada, loadb, asel, asel, loads, loadc;   
+  output [15:0] datapath_out; 
+  output Z_out; 
+
+  reg [15:0] data_in 
+
+  always_comb begin 
+    data_in = vsel ? datapath_in : datapath_out;
+    
+    regfile register(data_in, writenum, write, readnum, clk, data_out); 
+    
+  
 
 
-  // from ALU file 
-  // declarations from Lab 5 outline
-  input [15:0] Ain, Bin;
-  input [1:0] ALUop;
-  output reg [15:0] out;
-  output reg Z;
-
-  // from shifter file 
-  // declarations of inputs and output
-  input [15:0] in; // The 16-bit input that is to be shifted
-  input [1:0] shift; //The 2-bit contol signal for the shift operation
-  output reg [15:0] sout; //The 16-bit shifted output
   
   
   
