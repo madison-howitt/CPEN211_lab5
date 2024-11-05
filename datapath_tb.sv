ef1f3fe2f2b76b87e78e1614d313c33d7edbc1c2
madison-howitt <159974224+madison-howitt@users.noreply.github.com>
2024-11-05 13:32:11 -0800

    Update datapath_tb.sv

--- datapath_tb.sv
+++ datapath_tb.sv
@@ -3,7 +3,7 @@ module datapath_tb();
   reg [15:0] datapath_in; 
   reg [2:0] writenum, readnum;
   reg [1:0] shift, ALUop; 
-  reg vsel, write, clk, loada, loadb, asel, bsel, loads, loadc;
+  reg vsel, write, clk, loada, loadb, asel, bsel, loads, loadc, err;
 
   // declare outputs of top-level module 
   wire [15:0] datapath_out; 
@@ -37,7 +37,7 @@ module datapath_tb();
     end 
 
   initial begin 
-    
+    err = 0; 
     // TEST with video example
     
     datapath_in = 16'b0000000000101010;   // input 42
@@ -45,21 +45,28 @@ module datapath_tb();
     write = 1; 
     writenum = 3'b011;   // write to register R3
     #10;                 // wait one clock cycle
+    write = 0;           // reset write to avoid unintended writes
 
     datapath_in = 16'b0000000000001101;   // input 13
-    writenum = 3'b101;   // write to register R3
-    #10;                 // wait one clock cycle 
+    vsel = 1; 
+    write = 1; 
+    writenum = 3'b101;   // write to register R5
+    #10;                 // wait one clock cycle
+    write = 0;           // reset write to avoid unintended writes
 
     vsel = 0;
-    write = 0; 
 
-    readnum = 3'b011;   // load 42 from R3 to register B 
+    // load 42 from R3 to register B 
+    readnum = 3'b011; 
     loadb = 1;
     #10;                // wait one clock cycle 
-    loadb = 0; 
+    loadb = 0;          // reset loadb
 
     // check that the value was read correctly from the register
-    $display("data_out is %b, expected 16'b0000000000101010", dut.data_out);
+    if (dut.data_out !== 16'b0000000000101010) begin 
+      $display("ERROR: data_out is %b, expected 16'b0000000000101010", dut.data_out);
+      err = 1;
+    end
 
     readnum = 3'b101;   // load 13 from R5 to register A
     loada = 1; 
@@ -67,7 +74,10 @@ module datapath_tb();
     loada = 0; 
 
     // check that the value was read correctly from the register 
-    $display("data_out is %b, expected 16'b0000000000001101", dut.data_out);
+    if (dut.data_out !== 16'b0000000000001101) begin 
+      $display("ERROR: data_out is %b, expected 16'b0000000000001101", dut.data_out);
+      err = 1;
+    end
 
     // set ALU to add 42 + 13
     shift = 2'b0;   // no shift
@@ -77,20 +87,17 @@ module datapath_tb();
     loadc = 1;      // puting output of ALU into datapath_out so loadc is true 
     loads = 0; 
     #10;            // wait one clock cycle
+    loadc = 0;      // reset loadc
 
     // print output of top level module, should be the sum of 13 and 42 (= 55)
-    $display("datapath_out is %b, expected 16'b0000000000110111", dut.datapath_out);
+    if (dut.data_out !== 16'b0000000000110111) begin 
+      $display("ERROR: datapath_out is %b, expected 16'b0000000000110111", dut.datapath_out);
+      err = 1;
+    end
+
+    if (err == 0)
+      $display("Passed all tests!"); 
 
     $stop; 
   end 
-endmodule: datapath_tb 
-
-    
-    
-    
-
-    
-    
-    
-
-  
+endmodule: datapath_tb
